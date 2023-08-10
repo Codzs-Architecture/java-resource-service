eecd0053634ae1f7deb23d5f850bb2c700c2d17b
Nitin Khaitan <khaitan.nitin@gmail.com>
2023-08-11 07:11:37 +1000

    logback config updated

--- src/main/java/com/codzs/controller/ArticleExampleController.java
+++ src/main/java/com/codzs/controller/ArticleExampleController.java
@@ -1,12 +1,18 @@
 package com.codzs.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ArticleExampleController {
+    private static Logger LOGGER = LoggerFactory.getLogger(ArticleExampleController.class);
+
     @GetMapping("/articles")
     public String[] getArticles() {
+        LOGGER.info("Returning articles");
+
         return new String[] {
                 "Article 1",
                 "Article 2",
